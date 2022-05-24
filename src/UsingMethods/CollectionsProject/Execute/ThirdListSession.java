676122f84137e6a3069e6ad30ab62f638ee2d99c
devgalaxyUGC <bryansantos.fdtobc@gmail.com>
2022-05-24 11:22:58 -0300

    Third project Collections Java finished Commit

--- src/UsingMethods/CollectionsProject/Execute/ThirdListSession.java
+++ src/UsingMethods/CollectionsProject/Execute/ThirdListSession.java
@@ -3,8 +3,6 @@ package UsingMethods.CollectionsProject.Execute;
 
 import java.util.*;
 
-import static java.util.Locale.US;
-
 public class ThirdListSession {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,49 +12,58 @@ public class ThirdListSession {
         int count = 0;
 
         while (count < 6) {
-            System.out.println("Write down the temperature: ");
+            System.out.println("Write down the average temperature: ");
             double degreeEntrance = scanner.nextDouble();
             degreeStorage.add(degreeEntrance);
             count++;
         }
-        System.out.println("The first six months: "+degreeStorage);
+        System.out.println("The first six months: " + degreeStorage);
 
         Iterator<Double> sumTemperature = degreeStorage.iterator();
+
         double sumVariable = 0;
-        double sumAverage;
-        while(sumTemperature.hasNext()){
+        Double sumAverage;
+        while (sumTemperature.hasNext()) {
             Double next = sumTemperature.next();
             sumVariable += next;
         }
-        sumAverage = sumVariable/degreeStorage.size();
-        System.out.println("The average temperature is: "+sumAverage);
-        System.out.println("Minor: "+Collections.min(degreeStorage));
-        System.out.println("Greatest: "+Collections.max(degreeStorage));
+        sumAverage = sumVariable / degreeStorage.size();
+        System.out.println("The average temperature is: " + sumAverage);
+        System.out.println("Minor: " + Collections.min(degreeStorage));
+        System.out.println("Greatest: " + Collections.max(degreeStorage));
 
         // Arranjar algum modo de relacionar a temperatura mais alta com a média e com o switch abaixo!!!!
-        int monthsCounter = 0;
-        switch(monthsCounter){
-            case 0:
-                System.out.println("A maior temperatura foi no mês de Janeiro");
-                break;
-            case 1:
-                System.out.println("A maior temperatura foi no mês de Fevereiro");
-                break;
-            case 2:
-                System.out.println("A maior temperatura foi no mês de Março");
-                break;
-            case 3:
-                System.out.println("A maior temperatura foi no mês de Abril");
-                break;
-            case 4:
-                System.out.println("A maior temperatura foi no mês de Maio");
-                break;
-            case 5:
-                System.out.println("A maior temperatura foi no mês de Junho");
-                break;
-        }
+        Iterator<Double> eachTemperatureIterator = degreeStorage.iterator();
 
+        int countSwitch = 0;
+        while (eachTemperatureIterator.hasNext()) {
+            Double temperature = eachTemperatureIterator.next();
+            if (temperature > sumAverage) {
+                switch (countSwitch) {
+                    case 0:
+                        System.out.println("January had the temperature greatest than the average!");
+                        break;
+                    case 1:
+                        System.out.println("February had the temperature greatest than the average!");
+                        break;
+                    case 2:
+                        System.out.println("March had the temperature greatest than the average!");
+                        break;
+                    case 3:
+                        System.out.println("April had the temperature greatest than the average!");
+                        break;
+                    case 4:
+                        System.out.println("May had the temperature greatest than the average!");
+                        break;
+                    case 5:
+                        System.out.println("June had the temperature greatest than the average!");
+                        break;
+                    default:
+                        System.out.println("There's no temperature greatest than the average");
+                }
+            }
+        countSwitch++;
         }
-
     }
+
 }
